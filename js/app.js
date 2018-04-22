64cb73399d5410bef69a2f357d2661d65748c88b
drcox23 <drcox23@gmail.com>
2018-04-22 07:34:09 -1000

    exercise 1 complete

--- js/app.js
+++ js/app.js
@@ -6,6 +6,15 @@ The function will add the following content inside the paragraph with the id of 
 
 var bacon = "Shoulder turducken brisket, kevin swine andouille tri-tip salami tail ham sausage pork loin. Ribeye short loin rump kielbasa pork. Capicola short loin turducken corned beef tongue, chuck leberkas salami frankfurter. Kielbasa fatback pancetta, ground round meatball turducken jowl ribeye alcatra sirloin bacon corned beef beef ribs short loin. Pork belly spare ribs biltong corned beef meatball short ribs tongue alcatra swine drumstick. Biltong shankle kevin, cupim sirloin bresaola brisket. Tail pork belly biltong ball tip tri-tip, pig jerky cow pastrami prosciutto ;ground round bacon capicola tongue meatball.";
 
+var button1 = document.getElementById('one');
+button1.addEventListener('click', moreContent);
+
+function moreContent() {
+    var newP = document.createElement('p');
+    newP.innerHTML = bacon;
+    more.appendChild(newP);
+}
+
 
 //2. HTTP
 /*Create a function named `lessContent` that will initiate the `Show Less` link after clicking on it.
